b27fdc02229df49a9fcd7d3a8d6f7829ae5a9fac
sridhaarani <sridhaaranir074@gmail.com>
2020-09-29 18:27:00 +0530

    This process got success and it was created by me

---
+++ src/App1demo/AppTwo.java
@@ -0,0 +1,11 @@
+package App1demo;
+
+public class AppTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module App2Demo {
+}
